4c15dd46caba63d3dcbc65d76dc7715004b95f25
zyad27 <zyadt782@gmail.com>
2025-01-18 08:03:32 +0200

    home page

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const navBar = document.querySelector("#navBar")
+const subForm = document.querySelector("#subForm")
+const pageUp = document.querySelector("#pageUp")
+
+window.addEventListener("scroll", ()=>{
+    if ((window.scrollY) > 40) {
+        navBar.classList.add("shadow-sm")
+    }
+    else navBar.classList.remove("shadow-sm")
+
+})
+
+if(subForm){
+    subForm.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        if(!subForm.mail.value){
+            subForm.mail.nextSibling.nextSibling.innerHTML = `<p class="m-danger-color mt-2"> E-mail is required </p>`
+            return false
+        }
+        else if (!((subForm.mail.value).includes("@") && (subForm.mail.value).includes(".com"))){
+            subForm.mail.nextSibling.nextSibling.innerHTML = `<p class="m-danger-color mt-2"> mail should be like example@mail.com </p>`
+            return false
+        }
+        else{
+            subscibeForm.mail.nextSibling.nextSibling.innerHTML = " "
+            return true
+        }
+    })
+}
+
+if(pageUp){
+    window.addEventListener("scroll", ()=>{
+        if((window.scrollY) >= (window.innerHeight)){
+            pageUp.classList.remove("d-none")
+        }
+        else pageUp.classList.add("d-none")
+    })
+
+    pageUp.addEventListener("click", () =>{
+        window.scrollTo({
+            top: 0 ,
+            behavior: "smooth"
+        })
+    })
+}
